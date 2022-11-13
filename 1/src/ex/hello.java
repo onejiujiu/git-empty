44dd267a00cb0b4c0c4bd71667367f93f7744328
young <2281931303@qq.com>
2022-11-13 10:19:46 +0800

    push-aa

--- 1/src/ex/hello.java
+++ 1/src/ex/hello.java
@@ -8,6 +8,7 @@ public class hello {
         System.out.println("hot fixmmmmmm");
         System.out.println("master 11111");
         System.out.println("hot 2222222");
+        System.out.println("push  ");
 
 
     }
